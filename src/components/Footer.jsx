8122881e29f1eba8cdfa2e4f8225fcdb64dc98d0
Pato-24 <parguello24@gmail.com>
2025-09-03 15:00:08 -0300

    Cambio nombre a mi Panaderia

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => (
   <footer className="footer">
     <div>© 2025 Panadería Delicias. Todos los derechos reservados.</div>
-    <div>Contacto: info@panaderiadelicias.com | Tel: (011) 1234-5678</div>
+    <div>Contacto: info@panaderiadelicias.com | Tel: (381) 234-5678</div>
     <div>Hecho con React | UTN Programación 4</div>
   </footer>
 );
